4812cf237b29cf21ce994a4898185a469c65e6f9
arsen-py <ohanjanyanarsen840@gmail.com>
2024-11-09 01:01:03 +0400

    html

---
+++ lang.js
@@ -0,0 +1,19 @@
+const langArr = {
+    "about": {
+        "en": "About",
+        "ru": "О НАС",
+        "arm": "Մեր Մասին",
+    },
+
+    "contact":{
+        "en": "Contact Us",
+        "ru": "СВЯЖИТЕСЬ С НАМИ",
+        "arm": "Կապվել մեր հետ",
+    },
+
+    "menu":{
+        "en": "Menu",
+        "ru": "Меню",
+        "arm": "Մենյու",
+    },
+}
---
+++ tasty.js
@@ -0,0 +1,31 @@
+const select = document.querySelector('select');
+const allLang = ['en','ru','arm']
+
+select.addEventListener('change', changeURLLanguage)
+
+function changeURLLanguage(){
+    let lang = select.value;
+    location.href = window.location.pathname + "#" + lang;
+    location.reload();
+}
+
+// Changes the language
+function changeLanguage(){
+    let hash = window.location.hash;
+    hash = hash.substring(1);   // Gets hash(ru,arm,en)
+    if (!allLang.includes(hash)){
+        // If anything except (en,ru,arm) --> go to #en
+        location.href = window.location.pathname + "#en";
+        location.reload();
+    }
+    select.value = hash;
+    for (let key in langArr){
+        let elem = document.querySelector('#lng-' + key);
+        if (elem){
+            elem.innerHTML = langArr[key][hash];
+            // Changes our element to "hash" language
+        }
+    }
+}
+
+changeLanguage();
